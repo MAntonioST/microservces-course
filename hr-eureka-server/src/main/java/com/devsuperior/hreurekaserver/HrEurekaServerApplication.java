57ab392f2addc4d0cdd6abfede94cba551421a04
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-11-06 09:54:55 -0300

    Configurations

---
+++ hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.devsuperior.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import jdk.jfr.Enabled;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
